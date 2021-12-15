d00f4850bcfeb89dbb7a2e6ee2ad25c31de27601
Amarjit Yanglem <54314099+Aamjit@users.noreply.github.com>
2021-12-15 15:34:11 +0000

    whoami request returns  correct information

--- server.js
+++ server.js
@@ -31,3 +31,10 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get('/api/whoami', (req, res)=>{
+
+
+  res.json({ipaddress: req.connection.localAddress, language: req.headers["accept-language"], software: req.headers["user-agent"]});
+
+})
